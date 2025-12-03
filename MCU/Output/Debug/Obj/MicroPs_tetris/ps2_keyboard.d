ce2ad913f750e3ba200da438969e0e19e785ddb3
Noah-Fotenos <nfotenos@g.hmc.edu>
2025-12-03 14:02:18 -0800

    working

--- MCU/Output/Debug/Obj/MicroPs_tetris/ps2_keyboard.d
+++ MCU/Output/Debug/Obj/MicroPs_tetris/ps2_keyboard.d
@@ -1,15 +1,12 @@
-Output/Debug/Obj/MicroPs_tetris/main.o: \
- /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/main.c \
- /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/string.h \
+Output/Debug/Obj/MicroPs_tetris/ps2_keyboard.o: \
+ /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/ps2_keyboard.c \
+ /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/stdint.h \
  /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/__SEGGER_RTL.h \
  /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/__SEGGER_RTL_ConfDefaults.h \
  /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/__SEGGER_RTL_Conf.h \
  /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/__SEGGER_RTL_Arm_Conf.h \
- /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/stdlib.h \
  /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/stdio.h \
- /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/stdint.h \
- /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/main.h \
- /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/STM32L432KC.h \
+ /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/STM32L4xx/Device/Include/stm32l4xx.h \
  /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/STM32L4xx/Device/Include/stm32l432xx.h \
  /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/CMSIS_5/CMSIS/Core/Include/core_cm4.h \
  /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/CMSIS_5/CMSIS/Core/Include/cmsis_version.h \
@@ -17,15 +14,15 @@ Output/Debug/Obj/MicroPs_tetris/main.o: \
  /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h \
  /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/CMSIS_5/CMSIS/Core/Include/mpu_armv7.h \
  /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/STM32L4xx/Device/Include/system_stm32l4xx.h \
+ /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/ps2_keyboard.h \
+ /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/main.h \
+ /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/STM32L432KC.h \
+ /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/stdlib.h \
+ /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/STM32L4xx/Device/Include/stm32l432xx.h \
  /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/STM32L432KC_GPIO.h \
  /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/STM32L432KC_RCC.h \
  /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/STM32L432KC_TIM.h \
  /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/stdbool.h \
  /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/STM32L432KC_FLASH.h \
  /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/STM32L432KC_USART.h \
- /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/STM32L432KC_SPI.h \
- /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/random.h \
- /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/STM32L4xx/Device/Include/stm32l4xx.h \
- /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/STM32L4xx/Device/Include/stm32l432xx.h \
- /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/ps2_keyboard.h \
- /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/spi_protocol.h
+ /Users/noahfotenos/Desktop/E155-MicroPs/Project/MicroPs_Project/MCU/STM32L432KC_SPI.h
